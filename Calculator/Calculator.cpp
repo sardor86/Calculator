8a8abc11253800486696669a1a8e1b921b5f79b3
sardor86 <115871893+sardor86@users.noreply.github.com>
2023-10-02 15:20:45 +0500

    Update Calculator.cpp

--- Calculator/Calculator.cpp
+++ Calculator/Calculator.cpp
@@ -1,6 +1,3 @@
-﻿// Calculator.cpp: определяет точку входа для приложения.
-//
-
 #include "Calculator.h"
 
 using namespace std;
@@ -39,7 +36,17 @@ int main()
 	}
 	else if (action == "/")
 	{
+		if (second_number == 0)
+		{
+			cout << "\nError: First number is't divide";
+			return 1;
+		}
 		print_result(first_number / second_number);
+	}
+	else 
+	{
+		cout << "\nError: Action is incorrect";
+		return 2;
 	}
 
 	return 0;
