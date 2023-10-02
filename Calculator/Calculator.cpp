17a89b96cf03d95cbc95a9f5879afc43d4206357
sardor86 <sshavkatov900@gmail.com>
2023-10-02 15:05:08 +0500

    The first calculator in c++

---
+++ Calculator/Calculator.cpp
@@ -0,0 +1,46 @@
+﻿// Calculator.cpp: определяет точку входа для приложения.
+//
+
+#include "Calculator.h"
+
+using namespace std;
+
+void print_result(double result)
+{
+	cout << "\n" << "Result: " << result;
+}
+
+int main()
+{
+	long double first_number;
+	long double second_number;
+	string action;
+
+	cout << "First number: ";
+	cin >> first_number;
+
+	cout << "\nSecond number: ";
+	cin >> second_number;
+
+	cout << "\nAction: ";
+	cin >> action;
+
+	if (action == "+")
+	{
+		print_result(first_number + second_number);
+	}
+	else if(action == "-")
+	{
+		print_result(first_number - second_number);
+	}
+	else if(action == "*")
+	{
+		print_result(first_number * second_number);
+	}
+	else if (action == "/")
+	{
+		print_result(first_number / second_number);
+	}
+
+	return 0;
+}
